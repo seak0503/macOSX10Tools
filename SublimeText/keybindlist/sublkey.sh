e63a5067390367e8a4cb8e64977af7d6b25260c6
Shota Yamahira <seak0503@gmail.com>
2014-12-03 15:27:56 +0900

    Bug修正

--- SublimeText/keybindlist/sublkey.sh
+++ SublimeText/keybindlist/sublkey.sh
@@ -2,7 +2,7 @@
 
 ### 変数定義
 comment="# sublime textに設定したキーバインドを見る"
-alias_set="alias sublkey='less ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap'"
+alias_set="alias sublkey='less ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap'"
 
 ### .profileに設定を追加する
 echo "" >> ~/.profile
