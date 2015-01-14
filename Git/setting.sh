53113049450f16540c57ed1e98c17e39cf8d3830
Shota Yamahira <seak0503@gmail.com>
2015-01-14 17:59:20 +0900

    Git設定用のスクリプトを作成

---
+++ Git/setting.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#######################################################################
+# 解説
+#   gitの設定をするためのスクリプトです
+#
+# 前提条件
+#   条件1: gitがインストールされていること
+
+### ユーザへの各種質問
+echo -n "アカウント名は何ですか?:"
+read answer_account
+
+echo -n "メールアドレスを入力して下さい:"
+read answer_email
+
+### アカウント設定
+git config --global user.name "${answer_account}"
+git config --global user.email "${answer_email}"
+
+### カラー表示設定
+git config --global color.ui auto
+
+### gitコマンドのエイリアス設定
+git config --global alias.co checkout
+echo " "
+echo "git checkoutコマンドのエイリアス設定をgit coにしました"
+sleep 1
+
+### コミットメッセージ入力エディタの指定
+git confg --global core.editor "subl -w"
+echo " "
+echo "コミットメッセージ入力エディタをSublimeTextに設定しました"
+sleep 1
