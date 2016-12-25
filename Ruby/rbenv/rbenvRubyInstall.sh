51e0ce698eaf4ddd4ff16e2709bbb212acb57ea0
shyamahira <shyamahira@bbtower.co.jp>
2016-12-26 06:19:31 +0900

    rbenvのインストールスクリプト更新

--- Ruby/rbenv/rbenvRubyInstall.sh
+++ Ruby/rbenv/rbenvRubyInstall.sh
@@ -20,54 +20,14 @@
 ### rbenvをruby-buildのインストール
 brew update
 brew install rbenv ruby-build
+brew install rbenv-gemset
+brew install rbenv-gem-rehash
 
 ### 環境変数の設定
-echo 'eval "$(rbenv init -)"' >> ~/.profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
 source ~/.profile
 
-### インストール可能なRubyの一覧を確認
-ruby_Versions=$(rbenv install --list)
-
-### インストールするRubyユーザに確認する
-echo "インストール可能なRubyの一覧を表示します。"
-sleep 3
-echo " "
-for i in  $ruby_Versions; do
-  echo $i
-done
-echo " "
-echo -n "インストールするRubyのバージョンを指定して下さい。:"
-read answer
-grep -q $answer <(echo $ruby_Versions)
-if [ $? -ne 0 ]; then
-  echo "インストールできないバージョンが指定されました。"
-  exit 1
-fi
-
-### Rubyのインストール
-rbenv install $answer
-
-### 再読み込み
-rbenv rehash
-
-### インストールされたRubyのバージョン確認
-installed_ruby_virsion=$(rbenv versions)
-echo "インストールされたRubyのバージョンは $installed_ruby_virsion です。"
-
-### 環境全体のRubyバージョンを指定する
-rbenv global $(echo $installed_ruby_virsion | awk '{print $1}')
-
-### rbenv-gem-rehash(プラグイン)インストール
-git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-
-### rbenv-binstubs(プラグイン)インストール
-git clone git://github.com/ianheggie/rbenv-binstubs.git ~/.rbenv/plugins/rbenv-binstubs
-
-### rbenv-gemset(プラグイン)インストール
-git clone git://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
-
 ### インストール完了のコメント
-ruby_virsion=$(ruby -v)
 echo "インストールが完了しました。"
-echo "この環境で使用できるRubyのバージョンは $ruby_virsion です。"
 exit 0
