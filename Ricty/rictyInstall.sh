9cc717cddf9670479c1e0b310599edbfca2f00c4
shyamahira <shyamahira@bbtower.co.jp>
2016-12-26 19:56:17 +0900

    修正

--- Ricty/rictyInstall.sh
+++ Ricty/rictyInstall.sh
@@ -10,5 +10,5 @@ brew tap sanemat/font
 brew install Caskroom/cask/xquartz
 brew install ricty
 
-cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty*.ttf ~/Library/Fonts/
+cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
