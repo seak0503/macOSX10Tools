96d1221a26dbd365d199fbcc78ee5953d4e66071
shyamahira <shyamahira@bbtower.co.jp>
2016-12-26 23:19:45 +0900

    修正

--- SublimeText/keybindlist/sublkey.sh
+++ SublimeText/keybindlist/sublkey.sh
@@ -5,8 +5,8 @@ comment="# sublime textに設定したキーバインドを見る"
 alias_set="alias sublkey='less ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap'"
 
 ### .profileに設定を追加する
-echo "" >> ~/.profile
-echo $comment >> ~/.profile
-echo $alias_set >> ~/.profile
+echo "" >> ~/.zshrc
+echo $comment >> ~/.zshrc
+echo $alias_set >> ~/.zshrc
 
-source ~/.profile
+source ~/.zshrc
