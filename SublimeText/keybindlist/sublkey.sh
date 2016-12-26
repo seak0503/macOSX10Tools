014c3cd33709d454fb24384f3da61ae4da64b5d8
shyamahira <shyamahira@bbtower.co.jp>
2016-12-27 00:09:52 +0900

    SublimeText設定用スクリプト改修

--- SublimeText/keybindlist/sublkey.sh
+++ SublimeText/keybindlist/sublkey.sh
@@ -1,10 +1,15 @@
-# .profileにalias	を設定し、sublime textに設定したキーバインドをlessします。
+# .zschrcにSublimeText用のaliasを設定
 
-### 変数定義
-comment="# sublime textに設定したキーバインドを見る"
+### キーバインド確認用のalias設定
+comment="# SublimeTextに設定したキーバインドを見る"
 alias_set="alias sublkey='less ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap'"
+echo "" >> ~/.zshrc
+echo $comment >> ~/.zshrc
+echo $alias_set >> ~/.zshrc
 
-### .profileに設定を追加する
+### SublimeTextの設定ファイルがあるディレクトリへの移動用alias設定
+comment="# SublimeTextの設定ファイルに移動する"
+alias_set="alias cdSublime='cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User'"
 echo "" >> ~/.zshrc
 echo $comment >> ~/.zshrc
 echo $alias_set >> ~/.zshrc
