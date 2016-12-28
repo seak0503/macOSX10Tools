9d11effce8a8e455130f05066cf955a0fbe60195
Shota Yamahira <seak0503@gmail.com>
2016-12-28 12:19:30 +0900

    修正

--- Zsh/install.sh
+++ Zsh/install.sh
@@ -10,10 +10,10 @@ zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 ## 設定ファイルを作成
-$ setopt EXTENDED_GLOB
+setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 ## Shellのデフォルトに設定
-$ chsh -s /bin/zsh
+chsh -s /bin/zsh
